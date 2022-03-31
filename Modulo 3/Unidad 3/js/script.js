d935ee40750c06f00090375f0ecc781393f83ff4
Kevin Ruiz <kevinezer96@gmail.com>
2022-03-30 22:11:26 -0300

    M3U3JSParte1

---
+++ Modulo 3/Unidad 3/js/script.js
@@ -0,0 +1,22 @@
+// var valor1;
+// valor1 = prompt('Ingresar distancia:', '');
+
+// //Convercion a string.
+// // valor1 = parseInt(valor1);
+// // if (valor1 <= 1000) {
+// //     document.write('dirijase a pie');
+// // } else {
+// //     if (valor1 <= 10000) {
+// //         document.write('dirijase en bicicleta');
+// //     } else {
+// //         if (valor1 <= 30000) {
+// //             document.write('dirijase en colectivo');
+// //         } else {
+// //             if (valor1 <= 100000) {
+// //                 document.write('dirijase en auto');
+// //             } else {
+// //                 document.write('dirijase en avion');
+// //             }
+// //         }
+// //     }
+// // }
